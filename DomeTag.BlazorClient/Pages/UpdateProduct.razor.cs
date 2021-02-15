80d0abb1d077919b8c399f25210a29a95a584431
Ralph West <ralphwest1@gmail.com>
2021-02-15 09:18:10 -0700

    918a

--- DomeTag.BlazorClient/Pages/UpdateProduct.razor.cs
+++ DomeTag.BlazorClient/Pages/UpdateProduct.razor.cs
@@ -8,7 +8,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using DomeTag.API.SharedEntities.Models;
 
 namespace DomeTag.BlazorClient.Pages
 {
